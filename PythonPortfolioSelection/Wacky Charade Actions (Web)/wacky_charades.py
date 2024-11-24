0fa1d65e513e400d98e263477e4b97a044326907
a-dangranger <38672558+a-dangranger@users.noreply.github.com>
2024-11-24 12:32:29 -0600

    Adding Links to Github Projects

---
+++ PythonPortfolioSelection/Wacky Charade Actions (Web)/wacky_charades.py
@@ -0,0 +1,48 @@
+#Objective: Randomly Generate Funny Names
+
+#Components & Strategy:
+'''
+-Create data frame of various words
+-Words will be randomly paired.
+-User will need to intall Pandas and openpyxl
+-We will read from the 1st sheet which contains the first and last name. We will specifiy
+the column from which to pull that data. We will also pull from a different sheet which contains
+the middle names.
+-"Customer" Request: Ensure the output names font always appears in red.
+-Ensure the user can re-run the program until they choose to stop.
+-Ensure file notes/pseudocode adequately describe each segment of code.
+'''
+
+#Import
+import pandas as pd
+
+#Read Names Files
+adverbs_df = pd.read_excel("new_names_list.xlsx", sheet_name="Adverbs",usecols="A")
+Gerunds_df = pd.read_excel("new_names_list.xlsx", sheet_name="Words",usecols="A")
+Agent_Nounds_df = pd.read_excel("new_names_list.xlsx", sheet_name="Words",usecols="B")
+
+#Now we concatenate all the columns into one
+horizontal_concat = pd.concat([adverbs_df, Gerunds_df, Agent_Nounds_df], axis=1)
+horizontal_concat.dropna()
+
+#Define red font color
+RED = '\033[31m'
+
+#Create a reset to go back to standard font color
+RESET_TEXT = '\033[0m' # called to return to standard terminal text color
+
+#Print user message
+print("\nWelcome to the wacky Charades generator!")
+print("I'll give you a prompt and you act it out for your friends to guess.")
+print("Here's a prompt just for you:\n")
+
+
+while True:
+    Lname_output = horizontal_concat["Adverbs"].sample().values[0]
+    Mname_output = horizontal_concat["Gerunds"].sample().values[0]
+    Fname_output = horizontal_concat["Agent Nouns"].sample().values[0]
+    print("\n" + RED + f"{Fname_output} {Mname_output} {Lname_output}\n" + RESET_TEXT)
+
+    go_again = input("Would you like another wacky action?\nIf yes, enter y. Entering any other character will end our conversation.\n")
+    if go_again.lower() != 'y':
+        break
